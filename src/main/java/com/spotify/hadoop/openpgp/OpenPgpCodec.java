28630c184c3c2d44f0bd84910f181a7d20fcb4be
Tommie Gannert <tommie@spotify.com>
2011-08-14 22:36:58 +0200

    Make the OpenPgpCodec constructor add the BC security provider.

--- src/main/java/com/spotify/hadoop/openpgp/OpenPgpCodec.java
+++ src/main/java/com/spotify/hadoop/openpgp/OpenPgpCodec.java
@@ -3,12 +3,20 @@ package com.spotify.hadoop.openpgp;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.security.Security;
+
 import org.apache.hadoop.io.compress.CompressionInputStream;
 import org.apache.hadoop.io.compress.CompressionOutputStream;
 import org.apache.hadoop.io.compress.Compressor;
 import org.apache.hadoop.io.compress.Decompressor;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class OpenPgpCodec extends AbstractCompressionCodec {
+	public OpenPgpCodec() {
+		super();
+		ensureBouncyCastleProvider();
+	}
+
 	public Class<? extends Compressor> getCompressorType() {
 		return OpenPgpCompressor.class;
 	}
@@ -31,5 +39,10 @@ public class OpenPgpCodec extends AbstractCompressionCodec {
 
 	public String getDefaultExtension() {
 		return ".gpg";
+	}
+
+	protected void ensureBouncyCastleProvider() {
+		if (Security.getProvider("BC") == null)
+			Security.addProvider(new BouncyCastleProvider());
 	}
 }
