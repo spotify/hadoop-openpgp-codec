0499673d84a1bdbb8f470de41705137dd85c9449
Tommie Gannert <tommie@spotify.com>
2011-08-15 08:05:00 +0200

    Add test for a StreamCompressor stream that writes a header.

--- src/test/java/com/spotify/hadoop/openpgp/StreamCompressorTest.java
+++ src/test/java/com/spotify/hadoop/openpgp/StreamCompressorTest.java
@@ -1,5 +1,6 @@
 package com.spotify.hadoop.openpgp;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 import org.testng.annotations.*;
@@ -7,6 +8,9 @@ import static org.testng.AssertJUnit.*;
 
 
 public class StreamCompressorTest {
+	public static final String HEADER = "Header";
+	public static final byte[] HEADER_BYTES = HEADER.getBytes();
+
 	@Test
 	public void create() throws Exception {
 		StreamCompressor c = createIdentity();
@@ -95,9 +99,41 @@ public class StreamCompressorTest {
 		c.end();
 	}
 
+	@Test
+	public void headed() throws Exception {
+		StreamCompressor c = createHeaded();
+		byte[] b = "Hello World!".getBytes("UTF-8");
+
+		c.setInput(b, 0, 6);
+
+		// One byte larger to see we don't get garbage.
+		byte[] buf = new byte[HEADER_BYTES.length + b.length + 1];
+
+		assertEquals(HEADER_BYTES.length + 6, c.compress(buf, 0, buf.length));
+
+		c.setInput(b, 6, 6);
+		c.finish();
+
+		assertEquals(6, c.compress(buf, HEADER_BYTES.length + 6, 7));
+		assertEquals(HEADER + "Hello World!", new String(buf, 0, HEADER_BYTES.length + b.length, "UTF-8"));
+		c.end();
+	}
+
 	StreamCompressor createIdentity() throws Exception {
 		return new StreamCompressor(null) {
 			protected OutputStream createOutputStream(OutputStream out) {
+				return out;
+			}
+		};
+	}
+
+	StreamCompressor createHeaded() throws Exception {
+		return new StreamCompressor(null) {
+			protected OutputStream createOutputStream(OutputStream out) throws IOException {
+				out.write(HEADER_BYTES, 0, 3);
+				out.write(HEADER_BYTES[3]);
+				out.write(HEADER_BYTES, 4, HEADER_BYTES.length - 4);
+
 				return out;
 			}
 		};
